678f60fed1fb5ce911f087bb5082f9c701fb1071
Peachex <klevolex@gmail.com>
2020-01-26 22:23:55 +0300

    add fourth task

---
+++ src/com/epam/task4/Main.java
@@ -0,0 +1,62 @@
+package com.epam.task4;
+
+import java.util.Scanner;
+
+// 4. Даны действительные числа а 1  ,а 2  ,..., а n  . Поменять местами наибольший и наименьший элементы.
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("size: ");
+        int size = in.nextInt();
+
+        double[] array = new double[size];
+
+        for (int i = 0; i < size; i++) {
+            array[i] = (int) (Math.random() * 50 + 1) - 25;
+            System.out.print(array[i] + "  ");
+        }
+
+        int maxIndex = findMax(array), minIndex = findMin(array);
+        double temp;
+
+        System.out.println("\n\nmax = " + array[maxIndex] + "\nmin = " + array[minIndex] + "\n");
+
+        temp = array[maxIndex];
+        array[maxIndex] = array[minIndex];
+        array[minIndex] = temp;
+
+        for (int i = 0; i < size; i++) {
+            System.out.print(array[i] + "  ");
+        }
+        System.out.println();
+    }
+
+    public static int findMax(double[] array) {
+        double maxElement = array[0];
+        int maxPosition = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] > maxElement) {
+                maxElement = array[i];
+                maxPosition = i;
+            }
+        }
+
+        return maxPosition;
+    }
+
+    public static int findMin(double[] array) {
+        double minElement = array[0];
+        int minPosition = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            if (array[i] < minElement) {
+                minElement = array[i];
+                minPosition = i;
+            }
+        }
+
+        return minPosition;
+    }
+}
