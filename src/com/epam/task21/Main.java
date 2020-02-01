c1f023d34ec90a0d75688b5908c334cc80cff27d
Peachex <klevolex@gmail.com>
2020-02-01 15:47:55 +0300

    fix twentieth task

---
+++ src/com/epam/task21/Main.java
@@ -0,0 +1,42 @@
+package com.epam.task21;
+
+import java.util.Random;
+
+/* 11. Матрицу 10x20 заполнить случайными числами от 0 до 15. Вывести на экран саму матрицу и номера строк, в
+   которых число 5 встречается три и более раз. */
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        int[][] matrix = new int[10][20];
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 20; j++) {
+                matrix[i][j] = random.nextInt(16);
+                System.out.print(matrix[i][j] + "\t\t");
+            }
+            System.out.println();
+        }
+
+        System.out.println();
+
+        int count;
+        boolean flag = false;
+        for (int i = 0; i < 10; i++) {
+            count = 0;
+            for (int j = 0; j < 20; j++) {
+                if (matrix[i][j] == 5) {
+                    count++;
+                }
+            }
+            if (count >= 3) {
+                flag = true;
+                System.out.print((i + 1) + "\t");
+            }
+        }
+        if (flag) {
+            System.out.println(" - номера строк, в которых число 5 встречается три и более раз.");
+        } else {
+            System.out.println("Нет cтрок, в которых число 5 встречается три и более раз.");
+        }
+    }
+}
