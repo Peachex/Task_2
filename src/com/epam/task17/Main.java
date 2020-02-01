a27db9e5e3f8bee2e38ae1d4f68ec5f7e4544ec9
Peachex <klevolex@gmail.com>
2020-02-01 15:06:52 +0300

    fix seventeenth task

--- src/com/epam/task17/Main.java
+++ src/com/epam/task17/Main.java
@@ -2,7 +2,8 @@ package com.epam.task17;
 
 import java.util.Scanner;
 
-/* 7. Сформировать квадратную матрицу порядка N по правилу: A[I,J] = sin( ( (I)^2 - (J^2) ) / N). */
+/* 7. Сформировать квадратную матрицу порядка N по правилу: A[I,J] = sin( ( (I)^2 - (J^2) ) / N) и
+    подсчитать количество положительных элементов в ней. */
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -12,9 +13,13 @@ public class Main {
 
         double[][] matrix = new double[size][size];
 
+        int count = 0;
         for (int i = 0; i < size; i++) {
             for (int j = 0; j < size; j++) {
                 matrix[i][j] = matrixElement(i, j, size);
+                if (matrix[i][j] > 0) {
+                    count++;
+                }
             }
         }
 
@@ -24,6 +29,8 @@ public class Main {
             }
             System.out.println();
         }
+
+        System.out.println("\nКоличество положительных элементов: " + count);
     }
 
     public static double matrixElement(double i, double j, double size) {
---
+++ src/com/epam/task18/Main.java
@@ -0,0 +1,8 @@
+package com.epam.task18;
+
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
