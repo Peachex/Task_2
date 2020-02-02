e774c8c09f991457aa7c7e4d1214afb85fb78605
Peachex <klevolex@gmail.com>
2020-02-02 23:35:49 +0300

    add twenty third task

---
+++ src/com/epam/task23/Main.java
@@ -0,0 +1,70 @@
+package com.epam.task23;
+
+import java.util.Random;
+import java.util.Scanner;
+
+// 13. Отсотрировать стобцы матрицы по возрастанию и убыванию значений эементов.
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.print("size: ");
+        int size = in.nextInt();
+
+        int[][] matrix = new int[size][size];
+
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                matrix[i][j] = random.nextInt(29) + 1;
+                System.out.print(matrix[i][j] + "\t\t");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        System.out.print("Введите номер строки, относительно которого будет выполняться сортировка: ");
+        int number = in.nextInt() - 1, temp;
+
+        for (int count = 0; count < size - 1; count++) {
+            for (int j = 0; j < size - 1; j++) {
+                if (matrix[number][j] > matrix[number][j + 1]) {
+                    for (int i = 0; i < size; i++) {
+                        temp = matrix[i][j];
+                        matrix[i][j] = matrix[i][j + 1];
+                        matrix[i][j + 1] = temp;
+                    }
+                }
+            }
+        }
+
+        System.out.println("Сортировка столбцов матрицы по возрастанию значений элементов: ");
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(matrix[i][j] + "\t\t");
+            }
+            System.out.println();
+        }
+        System.out.println();
+
+        for (int count = 0; count < size - 1; count++) {
+            for (int j = 0; j < size - 1; j++) {
+                if (matrix[number][j] < matrix[number][j + 1]) {
+                    for (int i = 0; i < size; i++) {
+                        temp = matrix[i][j];
+                        matrix[i][j] = matrix[i][j + 1];
+                        matrix[i][j + 1] = temp;
+                    }
+                }
+            }
+        }
+        System.out.println("Сортировка столбцов матрицы по убыванию значений элементов: ");
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.print(matrix[i][j] + "\t\t");
+            }
+            System.out.println();
+        }
+        System.out.println();
+    }
+}
