4b6cadfcef0e955db10e616b8306738a1fc9f530
Peachex <klevolex@gmail.com>
2020-02-03 23:16:20 +0300

    add twenty fourth task

--- src/com/epam/task24/Main.java
+++ src/com/epam/task24/Main.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 
 /* 14. Сформировать случайную матрицу m x n, состоящую из нулей и единиц, причем в каждом столбце число
    единиц равно номеру столбца. */
-
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
