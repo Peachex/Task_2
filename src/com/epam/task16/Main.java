0184ff06b7ce4b239630fa21bfe3f3ef06a8fb71
Peachex <klevolex@gmail.com>
2020-02-05 18:19:53 +0300

    fix sixteenth task

--- src/com/epam/task16/Main.java
+++ src/com/epam/task16/Main.java
@@ -26,31 +26,17 @@ public class Main {
             }
         }
 
-        // Зануляем верхнюю левую половину матрицы.
-        for (int i = 1; i <= size / 2; i++) {
-            for (int j = 0; j < i; j++) {
-                matrix[i][j] = 0;
-            }
-        }
-
-        // Зануляем верхнюю правую половину матрицы.
-        for (int i = 1; i <= size / 2; i++) {
-            for (int j = size - 1; j >= size - i; j--) {
-                matrix[i][j] = 0;
-            }
-        }
-
-        // Зануляем нижнюю левую половину матрицы.
-        for (int i = size - 2; i >= size / 2; i--) {
-            for (int j = 0; j < size - 1 - i; j++) {
-                matrix[i][j] = 0;
-            }
-        }
-
-        // Зануляем нижнюю правую половину матрицы.
-        for (int i = size - 2; i >= size / 2; i--) {
-            for (int j = size - 1; j >= i + 1; j--) {
-                matrix[i][j] = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i < size / 2) {
+                    if (j < i || j > size - i - 1) {
+                        matrix[i][j] = 0;
+                    }
+                } else {
+                    if (j < size - i - 1 || j > i) {
+                        matrix[i][j] = 0;
+                    }
+                }
             }
         }
 
