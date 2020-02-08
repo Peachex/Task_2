ed3ae4329e5d74f3f3ccc88c6a5eee67469c3827
Peachex <klevolex@gmail.com>
2020-02-08 19:52:25 +0300

    add twenty sixth task

--- src/com/epam/task26/Main.java
+++ src/com/epam/task26/Main.java
@@ -232,4 +232,5 @@ public class Main {
 }
 /* Алгоритмы, по которым строился магический квадрат были взяты с сайтов:
   1) https://ru.m.wikihow.com/%D1%80%D0%B5%D1%88%D0%B8%D1%82%D1%8C-%D0%BC%D0%B0%D0%B3%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D0%B9-%D0%BA%D0%B2%D0%B0%D0%B4%D1%80%D0%B0%D1%82#.D0.A1.D0.BE.D0.B2.D0.B5.D1.82.D1.8B
-  2) http://www.natalimak1.narod.ru/metody6.htm */
+  2) http://www.natalimak1.narod.ru/metody6.htm
+*/
