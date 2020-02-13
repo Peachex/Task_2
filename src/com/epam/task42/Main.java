d4c92de208b988a65f34ce14f215c818bf46d0ec
Peachex <klevolex@gmail.com>
2020-02-13 19:37:43 +0300

    add forty second task

---
+++ src/com/epam/task42/Main.java
@@ -0,0 +1,56 @@
+package com.epam.task42;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/* 8. Задан массив D. Определить следующие суммы: D[l] + D[2] + D[3]; D[3] + D[4] + D[5]; D[4] +D[5] +D[6].
+   Пояснение. Составить метод(методы) для вычисления суммы трех последовательно расположенных элементов массива с номерами от k до m. */
+public class Main {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        Random random = new Random();
+
+        System.out.print("Размер массива: ");
+        int size = in.nextInt();
+
+        int[] array = new int[size];
+
+        System.out.println("Массив: ");
+        for (int i = 0; i < size; i++) {
+            array[i] = random.nextInt(21) - 10;
+            System.out.print(array[i] + "\t");
+        }
+
+        System.out.print("\nВведите k и m: ");
+        int k = in.nextInt();
+        int m = in.nextInt();
+
+        findSum(array, size, k, m);
+    }
+
+    public static void findSum(int[] array, int size, int k, int m) {
+        int sum = 0;
+
+        for (int i = k - 1; i < m - 1; i++) {
+            if (i + 3 <= m - 1) {
+                sum = plus(array, i);
+
+                System.out.print("\nСумма: " + array[i] + ", " + array[i + 1] + ", " + array[i + 2] + " = " + sum);
+
+                i += 2;
+            } else {
+                i -= i + 3 - (m - 1);
+                sum = plus(array, i);
+
+                System.out.print("\nСумма: " + array[i] + ", " + array[i + 1] + ", " + array[i + 2] + " = " + sum);
+
+                i += 2;
+            }
+            System.out.println();
+        }
+    }
+
+    public static int plus(int[] array, int i) {
+        return (array[i] + array[i + 1] + array[i + 2]);
+    }
+}
