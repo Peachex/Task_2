f0fb5c35419123d3bbacb2018cbbca18fa8669fa
Peachex <klevolex@gmail.com>
2020-09-18 20:31:35 +0300

    fix task26

--- src/com/epam/task26/Main.java
+++ src/com/epam/task26/Main.java
@@ -1,6 +1,5 @@
 package com.epam.task26;
 
-import java.util.Random;
 import java.util.Scanner;
 
 /* 16. Магическим квадратом порядка n называется квадратная матрица размера nxn, составленная из чисел 1, 2, 3,
@@ -12,200 +11,124 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        Random random = new Random();
-
-        System.out.print("size: ");
+        System.out.print("Input size:");
         int size = in.nextInt();
 
-        int[][] matrix = new int[size][size];
-        zeroMatrix(matrix, size);
-
-        if (size % 2 != 0) {
-            int number = 1;
-            magicSquare(matrix, size, number);
-
+        int[][] magicSquare = new int[size][size];
+        if (size % 2 !=0 ) {
+            magicSquare = createMagicSquareForEvenSize(size * 2, 1);
         } else {
-            if (size % 2 == 0 && size % 4 != 0) {
-                int size2 = size / 2;
-
-                int[][] matrix1 = new int[size2][size2];
-                zeroMatrix(matrix1, size2);
-
-                int number = 1;
-                magicSquare(matrix1, size2, number);
-
-                int[][] matrix2 = new int[size2][size2];
-                zeroMatrix(matrix2, size2);
-
-                magicSquare(matrix2, size2, number);
-
-                int[][] matrix3 = new int[size2][size2];
-                zeroMatrix(matrix3, size2);
-
-                magicSquare(matrix3, size2, number);
-
-                int[][] matrix4 = new int[size2][size2];
-                zeroMatrix(matrix4, size2);
-
-                magicSquare(matrix4, size2, number);
-
-                for (int i = 0; i < size2; i++) {
-                    for (int j = 0; j < size2; j++) {
-                        matrix[i][j] = matrix1[i][j];
-                    }
-                }
-
-                for (int i = 0; i < size2; i++) {
-                    for (int j = 0; j < size2; j++) {
-                        matrix[i + size2][j + size2] = matrix2[i][j];
-                    }
-                }
-
-                for (int i = 0; i < size2; i++) {
-                    for (int j = 0; j < size2; j++) {
-                        matrix[i][j + size2] = matrix3[i][j];
-                    }
-                }
-
-                for (int i = 0; i < size2; i++) {
-                    for (int j = 0; j < size2; j++) {
-                        matrix[i + size2][j] = matrix4[i][j];
-                    }
-                }
-
-                int[][] matrix0 = new int[size][size];
-                int p = size2 * size2;
-                for (int i = 0; i < size; i++) {
-                    for (int j = 0; j < size; j++) {
-                        if (i < size2 && j < size2) {
-                            matrix0[i][j] = 0;
-                        } else {
-                            if (i < size2 && j >= size2) {
-                                matrix0[i][j] = 2 * p;
-                            } else {
-                                if (i >= size2 && j < size2) {
-                                    matrix0[i][j] = 3 * p;
-                                } else {
-                                    if (i >= size2 && j >= size2) {
-                                        matrix0[i][j] = p;
-                                    }
-                                }
-                            }
-                        }
-                    }
-                }
-
-                int k = (size - 2) / 4;
-                int temp;
-                for (int i = 0; i < size2; i++) {
-                    for (int j = 0; j < size2; j++) {
-                        if (i != (size2 - 1) / 2) {
-                            if (j < k) {
-                                temp = matrix0[i][j];
-                                matrix0[i][j] = matrix0[i + size2][j];
-                                matrix0[i + size2][j] = temp;
-                            }
-                        } else {
-                            if (j <= (size2 - 1) / 2 && j >= ((size2 - 1) / 2) - (k - 1)) {
-                                temp = matrix0[i][j];
-                                matrix0[i][j] = matrix0[i + size2][j];
-                                matrix0[i + size2][j] = temp;
-                            }
-                        }
-                    }
-                }
-
-                for (int i = 0; i < size2; i++) {
-                    for (int j = size - 1; j > size - k; j--) {
-                        temp = matrix0[i][j];
-                        matrix0[i][j] = matrix0[i + size2][j];
-                        matrix0[i + size2][j] = temp;
-                    }
-                }
-
-                for (int i = 0; i < size; i++) {
-                    for (int j = 0; j < size; j++) {
-                        matrix[i][j] += matrix0[i][j];
-                    }
-                }
+            if (size % 4 != 0) {
+                magicSquare = createMagicSquareForOddEvenSize(size);
             } else {
-                if (size % 2 == 0 && size % 4 == 0) {
-                    int number1 = 0;
-                    int number2 = size * size + 1;
-                    for (int i = 0; i < size; i++) {
-                        for (int j = 0; j < size; j++) {
-                            number1++;
-                            number2--;
-                            if ((i < size / 4 || i > size - size / 4 - 1) && (j < size / 4 || j > size - size / 4 - 1)) {
-                                matrix[i][j] = number1;
-                            } else {
-                                if ((i >= size / 4 && i <= size - size / 4 - 1) && (j >= size / 4 && j < size - size / 4)) {
-                                    matrix[i][j] = number1;
-                                } else {
-                                    matrix[i][j] = number2;
-                                }
-                            }
-                        }
-                    }
-                }
+                magicSquare = createMagicSquareForOddOddSize(size);
             }
         }
 
-        viewMatrix(matrix, size);
-        System.out.println("This square is magic square: " + checkMagicSquare(matrix, size));
-    }
-
-    public static void magicSquare(int[][] matrix, int size, int number) {
-        int i = 0;
-        int j = (size + 1) / 2 - 1;
-        matrix[i][j] = number;
-        number++;
-        i--;
-        j++;
-
-        for (int count = 0; count < size * size - 1; count++, i--, j++, number++) {
-            if (i < 0 && j > size - 1) {
-                i += 2;
-                j -= 1;
-                matrix[i][j] = number;
-            } else {
-                if (i < 0) {
-                    i = size - 1;
-                    matrix[i][j] = number;
-                } else {
-                    if (j > size - 1) {
-                        j = 0;
-                        matrix[i][j] = number;
-                    } else {
-                        if (matrix[i][j] != 0) {
-                            i += 2;
-                            j -= 1;
-                            matrix[i][j] = number;
-                        } else {
-                            matrix[i][j] = number;
-                        }
-                    }
-                }
-            }
-        }
-    }
-
-    public static void zeroMatrix(int[][] matrix, int size) {
-        for (int i = 0; i < size; i++) {
-            for (int j = 0; j < size; j++) {
-                matrix[i][j] = 0;
-            }
-        }
-    }
-
-    public static void viewMatrix(int[][] matrix, int size) {
-        for (int a = 0; a < size; a++) {
-            for (int b = 0; b < size; b++) {
-                System.out.print(matrix[a][b] + "\t\t");
+        System.out.println("Is magic square: " + checkMagicSquare(magicSquare, size));
+        for (int[] i : magicSquare) {
+            for (int j : i) {
+                System.out.print(j + "\t");
             }
             System.out.println();
         }
-        System.out.println();
+    }
+
+    public static int[][] createMagicSquareForOddOddSize(int size) {
+        int[][] magicSquare = new int[size][size];
+        int count1 = size / 4;
+        int number1 = 1;
+        int number2 = (int) Math.pow(size, 2);
+        for (int i = 0; i < magicSquare.length; i++) {
+            for (int j = 0; j < magicSquare[i].length; j++) {
+                if (((i < count1 || i > magicSquare.length - count1 - 1) && (j < count1 || j > magicSquare.length - 1 - count1))
+                        || (i >= count1 && i <= magicSquare.length - count1 - 1) && (j >= count1 && j < magicSquare.length - count1)) {
+                    magicSquare[i][j] = number1++;
+                    number2--;
+                } else {
+                    magicSquare[i][j] = number2--;
+                    number1++;
+                }
+            }
+        }
+        return magicSquare;
+    }
+
+    public static void removeMagicSquareElementsForOddEvenSize(int[][] matrix2) {
+        int k = (matrix2.length - 2) / 4;
+        for (int i = 0; i < matrix2.length / 2; i++) {
+            if (i != matrix2.length / 2 / 2) {
+                for (int j = 0; j < k; j++) {
+                    int temp = matrix2[i][j];
+                    matrix2[i][j] = matrix2[matrix2.length / 2 + i][j];
+                    matrix2[matrix2.length / 2 + i][j] = temp;
+                }
+            } else {
+                for (int j = matrix2.length / 2 / 2; j >= matrix2.length / 2 / 2 - (k - 1); j--) {
+                    int temp = matrix2[i][j];
+                    matrix2[i][j] = matrix2[matrix2.length - (i + 1)][j];
+                    matrix2[matrix2.length - (i + 1)][j] = temp;
+                }
+            }
+        }
+        for (int i = 0; i < matrix2.length / 2; i++) {
+            for (int j = matrix2.length - 1; j >= matrix2.length - (k - 1); j--) {
+                int temp = matrix2[i][j];
+                matrix2[i][j] = matrix2[matrix2.length / 2 + i][j];
+                matrix2[matrix2.length / 2 + i][j] = temp;
+            }
+        }
+    }
+
+    public static int[][] createMagicSquareForOddEvenSize(int size) {
+        int[][] magicSquare = new int[size][size];
+        fillMagicSquareForOddEvenSize(0, 0, size / 2, size / 2, createMagicSquareForEvenSize(size, 1), magicSquare);
+        fillMagicSquareForOddEvenSize(0, size / 2, size / 2, size, createMagicSquareForEvenSize(size, (int) Math.pow(size, 2) / 2 + 1), magicSquare);
+        fillMagicSquareForOddEvenSize(size / 2, 0, size, size / 2, createMagicSquareForEvenSize(size, 1 + (int) Math.pow(size, 2) - (int) Math.pow(size, 2) / 4), magicSquare);
+        fillMagicSquareForOddEvenSize(size / 2, size / 2, size, size, createMagicSquareForEvenSize(size, (int) Math.pow(size, 2) / 4 + 1), magicSquare);
+        removeMagicSquareElementsForOddEvenSize(magicSquare);
+        return magicSquare;
+    }
+
+    public static void fillMagicSquareForOddEvenSize(int startI, int startJ, int endI, int endJ, int[][] magicSquare,
+                                                     int[][] firstMatrix) {
+        int countI = 0;
+        int countJ;
+        for (int i = startI; i < endI; i++, countI++) {
+            countJ = 0;
+            for (int j = startJ; j < endJ; j++, countJ++) {
+                firstMatrix[i][j] = magicSquare[countI][countJ];
+            }
+        }
+    }
+    public static int[][] createMagicSquareForEvenSize(int size, int number) {
+        size /= 2;
+        int[][] magicSquare = new int[size][size];
+        int rightPlaceI = 0;
+        int rightPlaceJ = size / 2;
+        int previousPlaceI;
+        int previousPlaceJ;
+        int count = 1;
+        magicSquare[0][size / 2] = number++;
+        while (count < (int) Math.pow(size, 2)) {
+            previousPlaceI = rightPlaceI;
+            previousPlaceJ = rightPlaceJ;
+            rightPlaceI--;
+            rightPlaceJ++;
+            if (rightPlaceI < 0) {
+                rightPlaceI = size - 1;
+            }
+            if (rightPlaceJ >= size) {
+                rightPlaceJ = 0;
+            }
+            if (magicSquare[rightPlaceI][rightPlaceJ] != 0) {
+                rightPlaceI = ++previousPlaceI;
+                rightPlaceJ = previousPlaceJ;
+            }
+            magicSquare[rightPlaceI][rightPlaceJ] = number++;
+            count++;
+        }
+        return magicSquare;
     }
 
     public static boolean checkMagicSquare(int[][] matrix, int size) {
@@ -236,7 +159,6 @@ public class Main {
         return true;
     }
 }
-/* Алгоритмы, по которым строился магический квадрат были взяты с сайтов:
-  1) https://ru.m.wikihow.com/%D1%80%D0%B5%D1%88%D0%B8%D1%82%D1%8C-%D0%BC%D0%B0%D0%B3%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D0%B9-%D0%BA%D0%B2%D0%B0%D0%B4%D1%80%D0%B0%D1%82#.D0.A1.D0.BE.D0.B2.D0.B5.D1.82.D1.8B
-  2) http://www.natalimak1.narod.ru/metody6.htm
+/*
+  Алгоритмы, по которым строился магический квадрат были взяты с сайта: https://ru.m.wikihow.com/%D1%80%D0%B5%D1%88%D0%B8%D1%82%D1%8C-%D0%BC%D0%B0%D0%B3%D0%B8%D1%87%D0%B5%D1%81%D0%BA%D0%B8%D0%B9-%D0%BA%D0%B2%D0%B0%D0%B4%D1%80%D0%B0%D1%82#.D0.A1.D0.BE.D0.B2.D0.B5.D1.82.D1.8B
 */
